9ded30195e9a94b4ee913ff3048019946f10cdb8
Philip <philip.kreissel@icloud.com>
2020-03-18 23:11:39 +0100

    fix API erros on subsequent requests

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from places import api_call
 sched = BlockingScheduler()
 
 #@sched.scheduled_job('cron', day_of_week='*', hour='9-21/1', misfire_grace_time=1000)
-@sched.scheduled_job('cron', day_of_week='*', hour='9-23/1', misfire_grace_time=1000)
+@sched.scheduled_job('cron', day_of_week='*', hour='9-21/3', misfire_grace_time=1000)
 def timed_job():
     print('This job is run everyday every 3 hours from 6-21h')
     api_call()
--- places.py
+++ places.py
@@ -4,6 +4,7 @@ from itertools import cycle
 from sqlalchemy import create_engine, Column, Integer, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import time
 
 
 #Configure Database
@@ -11,6 +12,7 @@ with open("database.txt") as f:
     engine = create_engine(f.readline())
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
 class Entry(Base):
     __tablename__ = 'entries'
     __table_args__ = {'extend_existing': True}
@@ -35,18 +37,31 @@ with open("api_keys.txt") as f:
 
 #Iterate over Places
 def api_call():
-    place_ids = ["ChIJuVGxxf51nkcRwhxFwvIr7EM"]
+    place_ids = ["ChIJuVGxxf51nkcRwhxFwvIr7EM", "ChIJ4dic-71RqEcRZIsmE3K6r-0", "ChIJmY9JK6Ulv0cRkNik00YUhZU", "ChIJ4aTfAYhXn0cRPJda3ksNq_0", "ChIJNTqGKTLbmUcRgQqjvTsO3wA", "ChIJI_Q3R-GOsUcRzz79A3qPxlw", "ChIJlSNuYVUJvUcRZmX1t81kEGU", "ChIJicfwQcrCuEcRBScpoj9SRJg", " ChIJm14qVOQZuUcR4XB5FsEB4zc", "ChIJ8bSeYiL4pkcRDYsuVPgKgps", " ChIJYRu0lIrFCUcRdTswsKeDvvs"]
     session = Session()
-    for place_id in place_ids:
-        data = populartimes.get_id(next(api_keys), place_id)
-        entry = Entry(
-            data["name"],
-            data["id"],
-            data["populartimes"][datetime.now().weekday()]["data"][datetime.now().hour],
-            data["current_popularity"]
-        )
-        session.add(entry)
+    for x, place_id in enumerate(place_ids):
+        print(x)
+        time.sleep(65)
+        try:
+            key = next(api_keys)
+            data = populartimes.get_id(key, place_id)
+        except Exception as e:
+            print(e)
+            print("Error with key: " + key)
+            continue
+        if "current_popularity" in data:
+            entry = Entry(
+                data["name"],
+                data["id"],
+                data["populartimes"][datetime.now().weekday()]["data"][datetime.now().hour],
+                data["current_popularity"]
+            )
+            session.add(entry)
+        else:
+            print("No Popularity-Data for " + data["name"])
     session.commit()
     #Lets us know how many Calls we have made so far.
     print("There are now " + str(session.query(Entry).count()) + " Entries in the Database")
     session.close()
+
+api_call()
