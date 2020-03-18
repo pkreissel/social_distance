eec7a2781a9c7ddedbf6cec90e9e1349bd717286
Philip <philip.kreissel@icloud.com>
2020-03-18 17:40:22 +0100

    fix typo

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import subprocess
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='*', ,hour='9-21/3')
+@sched.scheduled_job('cron', day_of_week='*', hour='9-21/3')
 def timed_job():
     print('This job is run everyday every 3 hours from 6-21h')
     subprocess.call('python places.py', shell=True, close_fds=True)
